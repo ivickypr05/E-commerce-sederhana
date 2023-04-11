e14b3c0fb19973a43a718c965c97029a588671e7
Siti Mutamimah <sitimutamimah669@gmail.com>
2023-04-11 20:02:12 +0700

    styling page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,65 @@
 @extends('layouts.main')
 @section('title', 'ReStyle Login | Page')
+
 @section('content')
-
-    <div class="w-50 center px-5 py-3 mx-auto">
-        <h1>
-            <center> Login Here</center>
-        </h1>
-        <div class="images-center">
-            <img src="https://img.pikbest.com/png-images/20191028/e-commerce-shopping-festival-shopping-gif-animation_2515297.png!bw700"
-                class="img-fluid" alt="Illustration Login">
+<div class="container"><br>
+<section class="" style="background-color: #9A616D;">
+    <div class="container py-10">
+      <div class="row d-flex justify-content-center align-items-center h-50">
+        <div class="col col-xl-11   ">
+          <div class="card" style="border-radius: 4rem;">
+            <div class="row g-6">
+              <div class="col-md-2 col-lg-5 d-md-block mt-5">
+                <img src="https://img.pikbest.com/png-images/20191028/little-girl-pushing-a-shopping-cart-to-buy-things-gif_2515300.png!bw700"
+                  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+              </div>   
+              <div class="col-md-6 col-lg-7 d-flex align-items-center"><br> 
+                  <form>
+                    <div class="d-flex align-items-center mb-3 pb-1">
+                    </div>
+                    <br>
+                    <div class="d-flex align-items-center mb-3 pb-1">
+                        <i style="color: #ff6219;"></i>
+                        <span class="h1 fw-bold"> ReStyle Fourteen</span>
+                      </div>
+                      <br>
+                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+    
+                    <form action="/auth/login" method="POST">
+                        @csrf
+                        <!-- Email input -->
+                        <div class="mb-3">
+                            <input type="email" id="email" class="form-control @error('email') is-invalid @enderror"
+                                placeholder="Email" name="email" value="{{ Session::get('email') }}" />
+                            @error('email')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+  
+                    <div class="mb-3">
+                        <input type="password" id="email" class="form-control @error('password') is-invalid @enderror"
+                            placeholder="Password" name="password" />
+                        @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+  
+                    <div class="pt-1 mb-4">
+                      <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                    </div>
+                      <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="register"
+                          style="color: #393f81;">Register here</a></p><br><br>
+                    </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <form action="/auth/login" method="POST">
-            @csrf
-            <!-- Email input -->
-            <div class="mb-3">
-                <input type="email" id="email" class="form-control @error('email') is-invalid @enderror"
-                    placeholder="Email" name="email" value="{{ Session::get('email') }}" />
-                @error('email')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <!-- Password input -->
-            <div class="mb-3">
-                <input type="password" id="email" class="form-control @error('password') is-invalid @enderror"
-                    placeholder="Password" name="password" />
-                @error('password')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            {{-- Button --}}
-            <div class="text-center text-lg-start pt-2 btn-sm d-grid">
-                <button type="submit" class="btn btn-dark "
-                    style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-                <p class="fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="register"
-                        class="link-warning">Register</a>
-                </p>
-            </div>
-        </form>
+      </div>
     </div>
+  </section><br>
 @endsection
