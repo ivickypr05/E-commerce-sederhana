f68e969d20a917c71fe1a6c6217eac3471eacc27
ivickypr05 <iko.pratama.ti.20@cic.ac.id>
2023-04-11 21:05:36 +0700

    Fix Problem Login Page and Navbar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,64 +2,67 @@
 @section('title', 'ReStyle Login | Page')
 
 @section('content')
-<div class="container"><br>
-<section class="" style="background-color: #9A616D;">
-    <div class="container py-10">
-      <div class="row d-flex justify-content-center align-items-center h-50">
-        <div class="col col-xl-11   ">
-          <div class="card" style="border-radius: 4rem;">
-            <div class="row g-6">
-              <div class="col-md-2 col-lg-5 d-md-block mt-5">
-                <img src="https://img.pikbest.com/png-images/20191028/little-girl-pushing-a-shopping-cart-to-buy-things-gif_2515300.png!bw700"
-                  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-              </div>   
-              <div class="col-md-6 col-lg-7 d-flex align-items-center"><br> 
-                  <form>
-                    <div class="d-flex align-items-center mb-3 pb-1">
-                    </div>
-                    <br>
-                    <div class="d-flex align-items-center mb-3 pb-1">
-                        <i style="color: #ff6219;"></i>
-                        <span class="h1 fw-bold"> ReStyle Fourteen</span>
-                      </div>
-                      <br>
-                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-    
-                    <form action="/auth/login" method="POST">
-                        @csrf
-                        <!-- Email input -->
-                        <div class="mb-3">
-                            <input type="email" id="email" class="form-control @error('email') is-invalid @enderror"
-                                placeholder="Email" name="email" value="{{ Session::get('email') }}" />
-                            @error('email')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
+    <div class="container"><br>
+        <section class="" style="background-color: #9A616D;">
+            <div class="container py-10">
+                <div class="row d-flex justify-content-center align-items-center h-50">
+                    <div class="col col-xl-11   ">
+                        <div class="card" style="border-radius: 4rem;">
+                            <div class="row g-6">
+                                <div class="col-md-2 col-lg-5 d-md-block mt-5">
+                                    <img src="https://img.pikbest.com/png-images/20191028/little-girl-pushing-a-shopping-cart-to-buy-things-gif_2515300.png!bw700"
+                                        alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
                                 </div>
-                            @enderror
-                        </div>
-  
-                    <div class="mb-3">
-                        <input type="password" id="email" class="form-control @error('password') is-invalid @enderror"
-                            placeholder="Password" name="password" />
-                        @error('password')
-                            <div class="invalid-feedback">
-                                {{ $message }}
+                                <div class="col-md-6 col-lg-7 d-flex align-items-center"><br>
+
+
+                                    <form action="/auth/login" method="POST">
+                                        @csrf
+                                        <div class="d-flex align-items-center mb-3 pb-1">
+                                        </div>
+                                        <br>
+                                        <div class="d-flex align-items-center mb-3 pb-1">
+                                            <i style="color: #ff6219;"></i>
+                                            <span class="h1 fw-bold"> ReStyle Fourteen</span>
+                                        </div>
+                                        <br>
+                                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account
+                                        </h5>
+                                        <!-- Email input -->
+                                        <div class="mb-3">
+                                            <input type="email" id="email"
+                                                class="form-control @error('email') is-invalid @enderror"
+                                                placeholder="Email" name="email" value="{{ Session::get('email') }}" />
+                                            @error('email')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <input type="password" id="password"
+                                                class="form-control @error('password') is-invalid @enderror"
+                                                placeholder="Password" name="password" />
+                                            @error('password')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="pt-1 mb-4">
+                                            <button class="btn btn-dark" type="submit">Login</button>
+                                        </div>
+                                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a
+                                                href="register" style="color: #393f81;">Register here</a></p><br><br>
+                                    </form>
+                                </div>
                             </div>
-                        @enderror
+                        </div>
                     </div>
-  
-                    <div class="pt-1 mb-4">
-                      <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
-                    </div>
-                      <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="register"
-                          style="color: #393f81;">Register here</a></p><br><br>
-                    </form>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </section><br>
+    </section><br>
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,39 +1,48 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-space-between" data-bs-theme="dark">
-    <div class="container">
-        <div class="collapse navbar-collapse">
-            <li class="navbar-nav ">
-                <h4>
-                    <a class="nav-link active" href="{{ route('home') }}">ReStle
-                        Fourteen</a>
-                </h4>
-            </li>
+<nav class="navbar navbar-expand-lg navbar-warning p-3">
+    <div class="container-fluid">
+        <li class="nav-item d-none d-lg-block">
+            <a class="nav-link mx-2" href="#">
+                <img src="https://i.postimg.cc/kXByRyS2/Screenshot-2023-04-10-192951.png" height="90" />
+            </a>
+        </li>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}"
-                            href="{{ route('home') }}">ReStye Product</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link {{ request()->routeIs('cart') ? 'active' : '' }}"
-                            href="{{ route('cart') }}">Cart</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link {{ request()->routeIs('transaction') ? 'active' : '' }}"
-                            href="{{ route('transaction') }}">Transactions</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="collapse navbar-collapse">
-                <li class=" navbar-nav nav-item dropdown">
-                    <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button"
-                        data-bs-toggle="dropdown">
-                        {{ Auth::user()->name }}
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a class="dropdown-item " href="/dashboard">Dashboard</a></li>
-                        <li><a class="dropdown-item " href="/logout">Logout</a></li>
-                </li>
-            </div>
+        <div class=" collapse navbar-collapse " id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto ">
+                <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item ">
+                            <a
+                                class="nav-link text-dark {{ request()->routeIs('home') ? 'active' : '' }}"href="{{ route('home') }}">ReStyle
+                                Product</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link text-dark{{ request()->routeIs('cart') ? 'active' : '' }}"
+                                href="{{ route('cart') }}">Cart</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link text-dark {{ request()->routeIs('transaction') ? 'active' : '' }}"
+                                href="{{ route('transaction') }}">Transactions</a>
+                        </li>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <div class="collapse navbar-collapse">
+                                    <li class=" navbar-nav nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle active text-dark" href="#"
+                                            id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown">
+                                            {{ Auth::user()->name }}
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                            <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
+                                            <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                                        </ul>
+                                    </li>
+                                </div>
+                        </div>
+                </div>
         </div>
+    </div>
 </nav>
