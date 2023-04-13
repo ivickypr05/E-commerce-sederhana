5ab0c299faca167360a040d44d45fdc4be7ccf77
ivickypr05 <iko.pratama.ti.20@cic.ac.id>
2023-04-13 14:11:57 +0700

    add SweetAlert and Styling App

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -111,6 +111,8 @@
                 </footer>
             </div>
         </div>
+        @include('sweetalert::alert')
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
         </script>
         <script src="{{ asset('assets/js/scripts.js') }}"></script>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,6 +27,8 @@
     <div class="container mt-4">
         @yield('content')
     </div>
+    @include('sweetalert::alert')
+
 </body>
 <!-- Bootstrap JavaScript Libraries -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
